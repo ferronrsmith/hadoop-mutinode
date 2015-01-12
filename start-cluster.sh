b6ec62528606e39150c9faf731a140e247abea53
Aaron Bell <aaron.bell@gmail.com>
2015-01-11 20:50:37 -0800

    scripts

--- start-cluster.sh
+++ start-cluster.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-docker run -d -t --dns 127.0.0.1 -e NODE_TYPE=s -P --name slave1 -h slave1.mycorp.kom alvinhenrick/hadoop-dn
-FIRST_IP=$(docker inspect --format="{{.NetworkSettings.IPAddress}}" slave1)
-docker run -i -t --dns 127.0.0.1 -e NODE_TYPE=m -e JOIN_IP=$FIRST_IP -P --name master -h master.mycorp.kom alvinhenrick/hadoop-nn-dn
+# launch slave
+docker run -d -t --dns 127.0.0.1 -e NODE_TYPE=s -P --name slave1 -h slave1.mycorp.kom mesosphere/hadoop-dn
 
+FIRST_IP=$(docker inspect --format="{{.NetworkSettings.IPAddress}}" slave1)
+
+# launch master in interactive mode
+docker run -i -t --dns 127.0.0.1 -e NODE_TYPE=m -e JOIN_IP=$FIRST_IP -P --name master -h master.mycorp.kom mesosphere/hadoop-nn-dn
+
+# next steps, executed in master shell:
+# /usr/local/hadoop/bin/start-hadoop.sh # all JVMs should come up successfully
+# su - hduser
+# hdfs dfs -ls /
