ccfc1865fdf7b8a01e3b219ab63e1586ccf9d043
Adam B <adam@mesosphere.io>
2015-01-13 03:15:17 -0800

    Start both containers with data volumes, detach

--- start-with-dfs-in-volume.sh
+++ start-with-dfs-in-volume.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-host_data_dir=/tmp/slave1_hdfs
+# Persistent locations on host
+host_data_dir_m=/tmp/hdfs/master
+host_data_dir_s=/tmp/hdfs/slave
 
-# Taken from dfs.datanode.data.dir in hdfs-site.xml - I confirmed that HDFS data lands here
+# Taken from dfs.datanode.data.dir in hdfs-site.xml
 hdfs_container_dir=/home/hduser/hdfs
 
-docker run -d -t --dns 127.0.0.1 -v $host_data_dir:$hdfs_container_dir -e NODE_TYPE=s -P --name slave1 -h slave1.mycorp.kom mesosphere/hadoop-dn
+docker run -d -t --dns 127.0.0.1 -v $host_data_dir_s:$hdfs_container_dir -P --name slave1 -h slave1.mycorp.kom mesosphere/hadoop-dn
 
 FIRST_IP=$(docker inspect --format="{{.NetworkSettings.IPAddress}}" slave1)
 
-docker run -i -t --dns 127.0.0.1 -e NODE_TYPE=m -e JOIN_IP=$FIRST_IP -P --name master -h master.mycorp.kom mesosphere/hadoop-nn-dn
+docker run -d -t --dns 127.0.0.1 -v $host_data_dir_m:$hdfs_container_dir -e JOIN_IP=$FIRST_IP -P --name master -h master.mycorp.kom mesosphere/hadoop-nn-dn
