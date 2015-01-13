929c76d02015b9befca29dff3cb9d38c8b7d7848
elingg <elingg@cs.stanford.edu>
2015-01-13 11:40:13 -0800

    Update hostname / add another slave node

--- start-cluster.sh
+++ start-cluster.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # launch slave
-docker run -d -t --dns 127.0.0.1 -e NODE_TYPE=s -P --name slave1 -h slave1.mycorp.kom mesosphere/hadoop-dn
+docker run -d -t --dns 127.0.0.1 -e NODE_TYPE=s -P --name slave1 -h slave1.hdfs.local mesosphere/hadoop-dn
 
 FIRST_IP=$(docker inspect --format="{{.NetworkSettings.IPAddress}}" slave1)
 
 # launch master in interactive mode
-docker run -i -t --dns 127.0.0.1 -e NODE_TYPE=m -e JOIN_IP=$FIRST_IP -P --name master -h master.mycorp.kom mesosphere/hadoop-nn-dn
+docker run -i -t --dns 127.0.0.1 -e NODE_TYPE=m -e JOIN_IP=$FIRST_IP -P --name master -h master.hdfs.local mesosphere/hadoop-nn-dn
 
 # next steps, executed in master shell:
 # /usr/local/hadoop/bin/start-hadoop.sh # all JVMs should come up successfully
--- start-with-dfs-in-volume.sh
+++ start-with-dfs-in-volume.sh
@@ -2,13 +2,16 @@
 
 # Persistent locations on host
 host_data_dir_m=/tmp/hdfs/master
-host_data_dir_s=/tmp/hdfs/slave
+host_data_dir_s1=/tmp/hdfs/slave1
+host_data_dir_s2=/tmp/hdfs/slave2
 
 # Taken from dfs.datanode.data.dir in hdfs-site.xml
 hdfs_container_dir=/home/hduser/hdfs
 
-docker run -d -t --dns 127.0.0.1 -v $host_data_dir_s:$hdfs_container_dir -P --name slave1 -h slave1.mycorp.kom mesosphere/hadoop-dn
+docker run -d -t --dns 127.0.0.1 -v $host_data_dir_s1:$hdfs_container_dir -P --name slave1 -h slave1.hdfs.local mesosphere/hadoop-dn
 
 FIRST_IP=$(docker inspect --format="{{.NetworkSettings.IPAddress}}" slave1)
+
+docker run -d -t --dns 127.0.0.1 -v $host_data_dir_s2:$hdfs_container_dir -e JOIN_IP=$FIRST_IP -P --name slave2 -h slave2.hdfs.local mesosphere/hadoop-dn
 
 docker run -d -t --dns 127.0.0.1 -v $host_data_dir_m:$hdfs_container_dir -e JOIN_IP=$FIRST_IP -P --name master -h master.mycorp.kom mesosphere/hadoop-nn-dn
