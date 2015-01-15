5e0d3673e48f55ea613d422dab01acdc5ce9d513
Adam B <adam@mesosphere.io>
2015-01-15 04:47:26 +0000

    Allow pre-set NN_IP variable

--- start-dn.sh
+++ start-dn.sh
@@ -2,6 +2,8 @@
 
 if [ -n $1 ]; then
   NN_IP_STR="-e NN_IP=$1"
+elif [ -n $NN_IP ]; then
+  NN_IP_STR="-e NN_IP=$NN_IP"
 fi
 
 docker run -d -t -v /tmp/hdfs/slave:/home/hduser/hdfs --net=host -P $NN_IP_STR --name datanode mesosphere/hadoop-datanode
