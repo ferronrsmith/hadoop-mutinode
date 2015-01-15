62fd54a6f6780567940eb1e6430a68b082f13ddc
Adam B <adam@mesosphere.io>
2015-01-15 04:11:51 +0000

    Configurable NN_IP for datanode

--- hadoop-datanode/config/start-datanode.sh
+++ hadoop-datanode/config/start-datanode.sh
@@ -6,6 +6,10 @@ if [ ! -d /home/hduser/hdfs/datanode ]; then
     chmod 700 /home/hduser/hdfs/datanode
 fi
 
+if [ -n $NN_IP ]; then
+    sed -i "s/0.0.0.0/$NN_IP/" $HADOOP_INSTALL/etc/hadoop/core-site.xml
+fi
+
 su hduser -c "$HADOOP_INSTALL/sbin/hadoop-daemon.sh start datanode"
 
 tail -f $HADOOP_INSTALL/logs/*
--- start-dn.sh
+++ start-dn.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-docker run -d -t -v /tmp/hdfs/slave:/home/hduser/hdfs --net=host -P --name datanode mesosphere/hadoop-datanode
+
+if [ -n $1 ]; then
+  NN_IP_STR="-e NN_IP=$1"
+fi
+
+docker run -d -t -v /tmp/hdfs/slave:/home/hduser/hdfs --net=host -P $NN_IP_STR --name datanode mesosphere/hadoop-datanode
